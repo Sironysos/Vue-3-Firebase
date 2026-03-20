273614ab0c57e19a0843cb2fbf69671318fcf2f2
alombard <alice.lombard@coexya.eu>
2026-03-20 14:22:16 +0100

    from Lesson 4 "create project" to Lesson 5 "emitting custom events"

--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal()" />
+  </div>
+  <button @click="toggleModal()">open Modal</button>
+  <!-- <input type="text" ref="name" />
+  <button @click="handleClick">click me</button> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Modal from './components/Modal.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    components: { Modal },
+    data() {
+      return {
+        title: 'My First Vue App!',
+        header: 'This is the header',
+        text: 'This is the text',
+        showModal: false
+      }
+    },
+    methods: {
+      /* handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+    } */
+      toggleModal() {
+        this.showModal = !this.showModal
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  h1 {
+    border-bottom: 1px solid #ddd;
+    display: inline-block;
+    padding-bottom: 10px;
+  }
 </style>
