1ad1d0ff812ef203c2b25e34045352c9e1e5d244
alombard <alice.lombard@coexya.eu>
2026-03-23 15:19:43 +0100

    Keyboard Events

--- web-form/src/components/SignUpForm.vue
+++ web-form/src/components/SignUpForm.vue
@@ -12,29 +12,20 @@
       <option value="designer">Web designer</option>
     </select>
 
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">{{ skill }}</div>
+
     <div class="terms">
       <input type="checkbox" v-model="terms" required />
       <label>Accept Terms and Conditions</label>
-    </div>
-
-    <div class="terms">
-      <input type="checkbox" value="Shaun" v-model="names" />
-      <label>Shaun</label>
-    </div>
-    <div class="terms">
-      <input type="checkbox" value="Yoshi" v-model="names" />
-      <label>Yoshi</label>
-    </div>
-    <div class="terms">
-      <input type="checkbox" value="Mario" v-model="names" />
-      <label>Mario</label>
     </div>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
   <p>Terms accepted? {{ terms }}</p>
-  <p>Names: {{ names }}</p>
+  <p>Skills: {{ skills.join(', ') }}</p>
 </template>
 
 <script>
@@ -45,7 +36,18 @@
         password: '',
         role: 'designer',
         terms: false,
-        names: []
+        tempSkill: '',
+        skills: []
+      }
+    },
+    methods: {
+      addSkill(e) {
+        if (e.key === ',' && this.tempSkill) {
+          if (!this.skills.includes(this.tempSkill)) {
+            this.skills.push(this.tempSkill)
+          }
+          this.tempSkill = ''
+        }
       }
     }
   }
