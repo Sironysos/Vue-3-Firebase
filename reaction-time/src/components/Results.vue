f046c1327623a9eae49161707a7cfbed83c45e79
alombard <alice.lombard@coexya.eu>
2026-03-23 12:26:04 +0100

    Ranking

--- reaction-time/src/components/Results.vue
+++ reaction-time/src/components/Results.vue
@@ -1,10 +1,27 @@
 <template>
-  <div class="results">Reaction time : {{ score }}ms</div>
+  <p class="results">Reaction time : {{ score }}ms</p>
+  <p class="rank">Rank : {{ rank }}</p>
 </template>
 
 <script>
   export default {
-    props: ['score']
+    props: ['score'],
+    data() {
+      return {
+        rank: null
+      }
+    },
+    mounted() {
+      if (this.score < 200) {
+        this.rank = 'Ninja'
+      } else if (this.score < 300) {
+        this.rank = 'Samurai'
+      } else if (this.score < 400) {
+        this.rank = 'Ronin'
+      } else {
+        this.rank = 'Snail pace...'
+      }
+    }
   }
 </script>
 
@@ -12,5 +29,9 @@
   .results {
     font-size: 24px;
     color: #0faf87;
+  }
+  .rank {
+    font-size: 18px;
+    color: #444;
   }
 </style>
