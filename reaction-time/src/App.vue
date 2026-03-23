c0840ac730ac9cb4f22d1b9037df8f3719f553a9
alombard <alice.lombard@coexya.eu>
2026-03-23 10:28:46 +0100

    Custom Events with Data

--- reaction-time/src/App.vue
+++ reaction-time/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay"></Block>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"></Block>
+  <p v-if="showResults">Reaction time : {{ score }}ms</p>
 </template>
 
 <script>
@@ -14,13 +15,21 @@
     data() {
       return {
         isPlaying: false,
-        delay: null
+        delay: null,
+        score: null,
+        showResults: false
       }
     },
     methods: {
       start() {
         this.delay = 2000 + Math.random() * 5000
         this.isPlaying = true
+        this.showResults = false
+      },
+      endGame(reactionTime) {
+        this.score = reactionTime
+        this.isPlaying = false
+        this.showResults = true
       }
     }
   }
